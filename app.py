395cee5eef0dd51e5dbf5476e8a4b50690496422
Steph <swdrinkard@gmail.com>
2021-03-15 22:52:59 -0500

    I AM MAD.

--- app.py
+++ app.py
@@ -1,29 +1,96 @@
-# 1. import Flask
+### Import dependencies ###
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
 from flask import Flask, jsonify
+#############################
 
-# 2. Create an app, being sure to pass __name__
+#############################
+### Set up database ###
+engine = create_engine("sqlite:///hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+##############################
+
+##############################
+### Create an app, being sure to pass __name__ ###
 app = Flask(__name__)
+##############################
 
-# 3. Define what to do when a user hits the index route
+##############################
+### Flask routes ###
+
+# Define what to do when a user hits the index route
 @app.route("/")
 def index():
-    return "Welcome to the 'Index' page!"
+    return (f"Welcome to the 'Index' page!<br/>"
+            f"Available routes:<br/>"
+            f"/api/v1.0/stations<br/>"
+            f"/api/v1.0/tobs<br/>"
+            f"/api/v1.0/precipitation<br/>"
+            f"/api/v1.0/<start>"
+            f"/api/v1.0/<start>/<end>")
 
 
-# 4. Define what to do when a user hits the /stations route
+# Define what to do when a user hits the /stations route
 @app.route("/api/v1.0/stations")
 def stations():
-    return "Welcome to the 'Stations' page!"
-
-# 5. Define what to do when a user hits the /tobs route
+    session = Session(engine)
+    results = session.query(Station.station).all()
+    session.close()
+            
+    all_stations = list(np.ravel(results))
+    return jsonify(all_stations)
+   
+            
+# Define what to do when a user hits the /tobs route
 @app.route("/api/v1.0/tobs")
-def stations():
-    return "Welcome to the 'Temperature Observations' page!"
+def tobs():
+    session = Session(engine)
+    results = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= '2016-08-23').\
+            filter(Measurement.station == 'USC00519281').all()
+    session.close()
 
-# 6. Define what to do when a user hits the /precipitation route
+    all_temps = []
+    for date, tobs in results:
+        temp_obs = {}   
+        temp_obs["date"] = date
+        temp_obs["tobs"] = tobs
+        all_temps.append(temp_obs)    
+    
+    return jsonify(all_temps) 
+            
+            
+# Define what to do when a user hits the /precipitation route
 @app.route("/api/v1.0/precipitation")
-def stations():
-    return "Welcome to the 'Precipitation' page!"
+def precipitation():
+    session = Session(engine)
+    results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= '2016-08-23').all()
+    session.close()
+
+    all_precip = []
+    for date, prcp in results:
+        precip_dict = {}   
+        precip_dict["date"] = date
+        precip_dict["prcp"] = tobs
+        all_precip.append(precip_dict)    
+    
+    return jsonify(all_precip) 
+
+# Define what to do when a user hits the /start and /start/end routes
+@app.route("api/v1.0/<start>")
+def variable_start(start):
+    session = Session(engine)
+    results = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= '2016-08-23').\
+            filter(Measurement.station == 'USC00519281').all()
+    session.close()
 
 
 
